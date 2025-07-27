d4678ef83615b0b8928087033795a5b9d94523a3
Kumail-rizvi110 <rizvikumail11099@gmail.com>
2025-07-27 14:22:44 +0500

    sign in loader work

--- Web/src/app/views/registration/registration.routes.ts
+++ Web/src/app/views/registration/registration.routes.ts
@@ -9,92 +9,97 @@ import { Contact } from 'lucide-angular';
 import { title } from 'process';
 import { TemporaryPatientDemographicListComponent } from './Temporary Patient Demographics/temporary-patient-demographic-list/temporary-patient-demographic-list.component';
 import { TemporaryDemographicsComponent } from './Temporary Patient Demographics/temporary-demographics/temporary-demographics.component';
+import { modulePermissionGuard } from '@core/guards/module-permission.guard';
 
 
 export const Registration_ROUTES: Routes = [
   {
     path: '',
     canActivate: [authGuard],
-    data: { permissions: ['Registration:Alerts']},
+    data: { permissions: ['Registration:Alerts'] },
     children: [
       {
-<<<<<<< HEAD
-            path: 'alerts',
-=======
+
         path: 'alerts',
->>>>>>> origin/main
+        canActivate: [modulePermissionGuard],
         component: AlertComponent,
         data: { title: 'Alerts' }
       },
-         {
+      {
         path: 'coverages',
+        canActivate: [modulePermissionGuard],
         component: CoverageListComponent,
         data: { title: 'Coverage List' }
       },
 
-        {
-          path: 'covrage-create',
-          component: CovrageCreateComponent,
-          data: { title: 'Coverage Create' }
-        },
+      {
+        path: 'covrage-create',
+        canActivate: [modulePermissionGuard],
+        component: CovrageCreateComponent,
+        data: { title: 'Coverage Create' }
+      },
 
-        {
+      {
         path: 'demographics',
+        canActivate: [modulePermissionGuard],
         component: DemographicsListComponent,
         data: { title: 'Demographic List' }
-        },
+      },
 
-        {
-          path: 'demographic-create',
-          component: DemographicCreateComponent,
-          data: { title: 'Demographic Create' }
-        },
+      {
+        path: 'demographic-create',
+        canActivate: [modulePermissionGuard],
+        component: DemographicCreateComponent,
+        data: { title: 'Demographic Create' }
+      },
 
-        {
-            path: 'temporary patient demographics',
-            component: TemporaryPatientDemographicListComponent,
-            data: { title: 'Temporary Patient Demographic List' },
-        },
-
-
-        {
-            path: 'temporary-demographics',
-            component: TemporaryDemographicsComponent,
-            data: { title: 'Temporary Demographics' },
-        },
+      {
+        path: 'temporary patient demographics',
+        canActivate: [modulePermissionGuard],
+        component: TemporaryPatientDemographicListComponent,
+        data: { title: 'Temporary Patient Demographic List' },
+      },
 
 
+      {
+        path: 'temporary-demographics',
+        canActivate: [modulePermissionGuard],
+        component: TemporaryDemographicsComponent,
+        data: { title: 'Temporary Demographics' },
+      },
 
 
 
 
 
-        //   {
-        //   path: 'coverages',
-        //   component: CoverageListComponent,
-        //   data: { title: 'Coverage List' }
-        // },
 
-    //   {
-    //     path: 'problem-list',
-    //     component: ProblemListComponent,
-    //     data: { title: 'Problem List' }
-    //   },
-    //   {
-    //     path: 'favorites',
-    //     component: FavoritesComponent,
-    //     data: { title: 'Favorites' }
-    //   },
-    //   {
-    //     path: 'problem',
-    //     component: ProblemComponent,
-    //     data: { title: 'problem-list' }
-    //   },
-    //   {
-    //     path: 'allergies',
-    //     component: AllergiesComponent,
-    //     data: { title: 'Allergies' }
-    //   }
+
+      //   {
+      //   path: 'coverages',
+      //   component: CoverageListComponent,
+      //   data: { title: 'Coverage List' }
+      // },
+
+      //   {
+      //     path: 'problem-list',
+      //     component: ProblemListComponent,
+      //     data: { title: 'Problem List' }
+      //   },
+      //   {
+      //     path: 'favorites',
+      //     component: FavoritesComponent,
+      //     data: { title: 'Favorites' }
+      //   },
+      //   {
+      //     path: 'problem',
+      //     component: ProblemComponent,
+      //     data: { title: 'problem-list' }
+      //   },
+      //   {
+      //     path: 'allergies',
+      //     component: AllergiesComponent,
+      //     data: { title: 'Allergies' }
+      //   }
 
     ]
   }
